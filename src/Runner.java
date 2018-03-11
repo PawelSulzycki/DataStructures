6385de17f4ac80466b943f1fd6984667a60ce56a
psulzycki <sulzyckipawel@gmail.com>
2018-03-12 00:55:05 +0100

    create queue

--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,15 @@
 public class Runner {
 
     public static void main(String[] args) {
-        Stack stack = new Stack(10);
+/*        Stack stack = new Stack(10);
         stack.push(10);
         stack.push(2);
-        stack.display();
+        stack.display();*/
+
+        Queue queue = new Queue(5);
+        queue.push(3);
+        queue.display();
+        queue.pop();
+        queue.display();
     }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -4,66 +4,66 @@ public class Stack {
 
     public Stack(int maxSize) {
         tab = new double[maxSize];
-        firstFree =0;
+        firstFree = 0;
     }
 
-    int getMaximumStackSize(){
+    int getMaximumStackSize() {
         return tab.length;
     }
 
-    boolean isEmpty(){
-        if(firstFree == 0)
+    boolean isEmpty() {
+        if (firstFree == 0)
             return true;
         else
             return false;
     }
 
-    int getSize(){
+    int getSize() {
         return firstFree;
     }
 
-    void push(double element) throws ArrayIndexOutOfBoundsException{
-        if(firstFree < tab.length){
+    void push(double element) throws ArrayIndexOutOfBoundsException {
+        if (firstFree < tab.length) {
             tab[firstFree] = element;
             firstFree++;
-        }else {
+        } else {
             throw new ArrayIndexOutOfBoundsException(
                     "Przepelnienie stosu, operacja nie powodła się");
         }
     }
 
-    double pop() throws IndexOutOfBoundsException{
-        if(firstFree < 0){
+    double pop() throws IndexOutOfBoundsException {
+        if (firstFree < 0) {
             throw new IndexOutOfBoundsException(
                     "Stos jest pusty, operacja nie powiodła się");
         }
         double elementStack = tab[firstFree - 1];
         firstFree--;
-        return  elementStack;
+        return elementStack;
     }
 
-    void display() throws IndexOutOfBoundsException{
-        if(firstFree == 0){
+    void display() throws IndexOutOfBoundsException {
+        if (firstFree == 0) {
             throw new IndexOutOfBoundsException(
                     "Stos jest pusty, operacja nie powiodła sie");
         }
-        int temp = firstFree -1;
+        int temp = firstFree - 1;
         do {
             System.out.println(tab[temp]);
             temp--;
-        }while (temp > -1);
+        } while (temp > -1);
     }
 
-    void multiPop(int amountElements){
-        if(amountElements < firstFree){
-            for(int i=amountElements; i>0;i--){
+    void multiPop(int amountElements) {
+        if (amountElements < firstFree) {
+            for (int i = amountElements; i > 0; i--) {
                 System.out.println(pop());
             }
         }
     }
 
-    void clear(){
-        for (int i=0; i<firstFree; i++){
+    void clear() {
+        for (int i = 0; i < firstFree; i++) {
             tab[i] = 0.0;
         }
         firstFree = 0;
