33a1b35fa2ee05f9c2c0751090d0bf48b340a8a0
psulzycki <sulzyckipawel@gmail.com>
2018-03-11 23:52:20 +0100

    create project and add stack

---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(10);
+        stack.push(10);
+        stack.push(2);
+        stack.display();
+    }
+}
